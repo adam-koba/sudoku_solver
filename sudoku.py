454aff769139496183d9e944e5ade7e8f1ff236b
Adam Koba <adamkoba@icloud.com>
2020-07-13 16:24:11 +0200

    Finished algorithm. Text version
    Backtracking algorithm finished. It is only text version, but doing it is doing its job

--- sudoku.py
+++ sudoku.py
@@ -12,13 +12,60 @@ board = [
     [0,8,4,7,2,0,0,0,0]
 ]
 
+def solve(bo):
+    # finding empty spaces. if they have not been found it will return True
+
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        # looping with numbers from 1 to 9. If loop through whole range and does not find solution return false and backtrack.
+        # If after looping will find some number, will write it to the free box
+        if validating(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+def validating(bo, numb, pos):
+    # function checking rows, columns and little squares
+
+    # check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == numb and pos[1] != i: # pos[0][i] -> rows, col
+            return False
+
+    # check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == numb and pos[0] != i:
+            return False
+
+    # check little square
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_x * 3 + 3):
+        for j in range(box_x * 3, box_y * 3 + 3):
+            if bo[i][j] == numb and (i, j) != pos:
+                return False
+
+    return True
+
+
 def print_board(bo):
 
 # printing board function
 
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
+            print("- - - - - - - - - - - -")
     # loop for dividing puzzle after 3 rows. to make it more readable
 
         for j in range(len(bo[0])):
@@ -43,4 +90,7 @@ def find_empty(bo):
 
     return None
 
+print_board(board)
+solve(board)
+print('-----------------------')
 print_board(board)
